9b8cf26519fa1f454e2589e47c55500f993f0989
reberthelot <remiberthelot49@gmail.com>
2025-11-21 15:52:51 +0100

    init update + test working
    Previous deleted file is required finally.

--- init_train_test.sh
+++ init_train_test.sh
@@ -1,3 +1,5 @@
-python  util/data_cropper.py --input_dir ./datasets/ifr_images/original --output_dir ./datasets/ifr_images
-python train.py --dataroot ./datasets/ifr_images --name ifr_model --model cycle_gan --n_epochs 10 --n_epochs_decay 10
-python test.py  --dataroot ./datasets/ifr_images --name ifr_model 
+python  util/data_cropper_average.py --average True
+python train.py --dataroot ./datasets/ifr_images --name ifr_model_unet --model cycle_gan --n_epochs 0 --n_epochs_decay 1 --init_type xavier --netG unet_256 --netD basic
+python test.py  --dataroot ./datasets/ifr_images --name ifr_model_unet 
+python test.py --dataroot ./datasets/ifr_images/testA --name ifr_model_unet --model_suffix _A --no_dropout
+python test.py --dataroot ./datasets/ifr_images/testB --name ifr_model_unet --model_suffix _B --no_dropout
