85baf1c58d63ea7d7ac643298b4057f383b544f2
reberthelot <remiberthelot49@gmail.com>
2025-12-01 14:21:01 +0100

    Andreas .py + addition for the .sh

--- init_train_test.sh
+++ init_train_test.sh
@@ -3,3 +3,8 @@ python train.py --dataroot ./datasets/ifr_images --name ifr_model_resnet_square 
 python test.py  --dataroot ./datasets/ifr_images --name ifr_model_resnet 
 python test.py --dataroot ./datasets/ifr_images/testA --name ifr_model_resnet --model_suffix _A --no_dropout
 # python test.py --dataroot ./datasets/ifr_images/testB --name ifr_model_unet --model_suffix _B --no_dropout
+
+
+python train.py --dataroot ./datasets/mock_images --name mock_model_resnet_1 --model cycle_gan  --init_type xavier --netG resnet_6blocks --n_epochs 8 --n_epochs_decay 16 --batch_size 8 --lambda_B 5 --pool_size 80 --preprocess none
+python test.py --dataroot ./datasets/mock_images/testA --name mock_model_resnet_1 --model_suffix _A --no_dropout
+python test.py --dataroot ./datasets/mock_images/testA --name ifr_model_resnet --model_suffix _A --no_dropout
